e9ddd6977f1190a539cdda775a4b7c4ead055810
Tim deBoer <git@tdeboer.ca>
2025-02-07 17:38:02 -0500

    chore: clean up hardcoded colors
    There are just a few remaining hard-coded colors to be removed,
    then the tailwind palette can be cleaned up so that they're not
    accidentally used again in the future.
    
    Part of #975.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/frontend/tailwind.config.cjs
+++ packages/frontend/tailwind.config.cjs
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (C) 2024 Red Hat, Inc.
+ * Copyright (C) 2024-2025 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,40 +55,7 @@ module.exports = {
       }),
     },
     colors: {
-      'charcoal': {
-        600: '#27272a',
-        800: '#18181b',
-      },
-      'gray': {
-        400: '#d1d1d1',
-        700: '#aaabac',
-        900: '#818181',
-      },
-      'purple': {
-        400: '#ad8bfa',
-        500: '#8b5cf6',
-      },
-      'sky': {
-        400: '#51a2da',
-      },
-      'green': {
-        600: '#2b7037',
-      },
-      'red': {
-        600: '#e5421d',
-      },
-      'amber': {
-        600: tailwindColors.amber[600],
-      },
       transparent: 'transparent',
-      black: '#000',
-      white: '#fff',
-      // The remaining colors below are not part of our palette and are only here
-      // to maintain existing code. No new use.
-      'violet': {
-        500: tailwindColors.violet[500],
-        600: tailwindColors.violet[600],
-      },
     },
   },
   plugins: [
